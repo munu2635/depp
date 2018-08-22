38b5757d5cc3e866f0c701c33ba06ef8e7064146
munu2635@naver.com <munu2635@naver.com>
2018-08-22 14:27:16 +0900

    data

---
+++ EX205_mnist_2.0_five_layers_sigmoid_graph.py
@@ -0,0 +1,146 @@
+# encoding: UTF-8
+# Copyright 2016 Google.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+import tensorflowvisu
+import mnistdata
+print("Tensorflow version " + tf.__version__)
+import matplotlib.pyplot as plt
+import numpy as np
+
+tf.set_random_seed(0)
+
+# neural network with 5 layers
+#
+# · · · · · · · · · ·          (input data, flattened pixels)       X [batch, 784]   # 784 = 28*28
+# \x/x\x/x\x/x\x/x\x/       -- fully connected layer (sigmoid)      W1 [784, 200]      B1[200]
+#  · · · · · · · · ·                                                Y1 [batch, 200]
+#   \x/x\x/x\x/x\x/         -- fully connected layer (sigmoid)      W2 [200, 100]      B2[100]
+#    · · · · · · ·                                                  Y2 [batch, 100]
+#     \x/x\x/x\x/           -- fully connected layer (sigmoid)      W3 [100, 60]       B3[60]
+#      · · · · ·                                                    Y3 [batch, 60]
+#       \x/x\x/             -- fully connected layer (sigmoid)      W4 [60, 30]        B4[30]
+#        · · ·                                                      Y4 [batch, 30]
+#         \x/               -- fully connected layer (softmax)      W5 [30, 10]        B5[10]
+#          ·                                                        Y5 [batch, 10]
+
+# Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
+mnist = mnistdata.read_data_sets("data", one_hot=True, reshape=False)
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+# correct answers will go here
+Y_ = tf.placeholder(tf.float32, [None, 10])
+
+# five layers and their number of neurons (tha last layer has 10 softmax neurons)
+L = 200
+M = 100
+N = 60
+O = 30
+# Weights initialised with small random values between -0.2 and +0.2
+# When using RELUs, make sure biases are initialised with small *positive* values for example 0.1 = tf.ones([K])/10
+W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  # 784 = 28 * 28
+B1 = tf.Variable(tf.zeros([L]))
+W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
+B2 = tf.Variable(tf.zeros([M]))
+W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
+B3 = tf.Variable(tf.zeros([N]))
+W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
+B4 = tf.Variable(tf.zeros([O]))
+W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
+B5 = tf.Variable(tf.zeros([10]))
+
+# The model - sigmoid 
+#XX = tf.reshape(X, [-1, 784])
+#Y1 = tf.nn.sigmoid(tf.matmul(XX, W1) + B1)
+#Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)
+#Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
+#Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
+#Ylogits = tf.matmul(Y4, W5) + B5
+#Y = tf.nn.softmax(Ylogits)
+
+# - RELU for slow start improve
+XX = tf.reshape(X, [-1, 784])
+Y1 = tf.nn.relu(tf.matmul(XX, W1) + B1)
+Y2 = tf.nn.relu(tf.matmul(Y1, W2) + B2)
+Y3 = tf.nn.relu(tf.matmul(Y2, W3) + B3)
+Y4 = tf.nn.relu(tf.matmul(Y3, W4) + B4)
+Ylogits = tf.matmul(Y4, W5) + B5
+Y = tf.nn.softmax(Ylogits)
+
+# cross-entropy loss function (= -sum(Y_i * log(Yi)) ), normalised for batches of 100  images
+# TensorFlow provides the softmax_cross_entropy_with_logits function to avoid numerical stability
+# problems with log(0) which is NaN
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
+cross_entropy = tf.reduce_mean(cross_entropy)*100
+
+# accuracy of the trained model, between 0 (worst) and 1 (best)
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+# training step, learning rate = 0.003
+learning_rate = 0.003
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+
+# init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+
+train_acc_list = []
+test_acc_list = []
+train_loss_list = []
+test_loss_list = []
+# run
+for i in range(2000+1) :
+
+    batch_X, batch_Y = mnist.train.next_batch(100)
+    a, c = sess.run([accuracy, cross_entropy], feed_dict={X : batch_X, Y_ : batch_Y})
+    print("training : ", i, ' accuracy = ', '{:7.4f}'.format(a), ' loss = ', c)
+    train_acc_list.append(a)
+    train_loss_list.append(c)
+
+    # test_batch_X, test_batch_Y = mnist.test.next_batch(100)  ==> never use mini batch!!
+    # sess.run(train_step, feed_dict={X: test_batch_X, Y_: test_batch_Y})  ==> never run train_step on test data!!
+    a, c = sess.run([accuracy, cross_entropy], feed_dict={X: mnist.test.images, Y_: mnist.test.labels})
+    print("testing  : ",i, ' accuracy = ', '{:7.4f}'.format(a), ' loss = ', c)
+    test_acc_list.append(a)
+    test_loss_list.append(c)
+
+    sess.run(train_step, feed_dict={X : batch_X, Y_ : batch_Y} )
+
+# draw graph : accuracy
+x = np.arange(len(train_acc_list))
+plt.figure(1) 
+plt.plot(x, train_acc_list,  label='train', markevery=1)
+plt.plot(x, test_acc_list, label='test', markevery=1)
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+# plt.show()
+
+# draw graph : loss
+x = np.arange(len(train_loss_list))
+plt.figure(2) 
+plt.plot(x, train_loss_list,  label='train', markevery=1)
+plt.plot(x, test_loss_list, label='test', markevery=1)
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.ylim(0, 100)
+plt.legend(loc='upper right')
+plt.show()
